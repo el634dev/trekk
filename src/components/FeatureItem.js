65128a902fa9bc2a4f58234cd744ba7465f4a854
el634dev <al7758066@gmail.com>
2024-08-12 20:12:12 -0400

    Added alt text for features

--- src/components/FeatureItem.js
+++ src/components/FeatureItem.js
@@ -1,18 +1,16 @@
 "use client"
 import React from 'react';
 
-function FeatureItem({ icon, text }) {
+export default function FeatureItem({ icon, text }) {
   return (
     <div className="flex gap-1 self-stretch mt-3 first:mt-0">
         <img
             loading="lazy" 
             src={icon} 
-            alt="" 
+            alt={feature.text}
             className="object-contain shrink-0 w-5 aspect-square" 
         />
       <div className="flex-auto">{text}</div>
     </div>
   );
-}
-
-export default FeatureItem;
+};
